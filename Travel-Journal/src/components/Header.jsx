be51e52087d70007b971e684590544eb27003f68
Vivek <vivekkad2002@gmail.com>
2025-10-15 16:35:04 +0530

    Update Header.jsx

--- Travel-Journal/src/components/Header.jsx
+++ Travel-Journal/src/components/Header.jsx
@@ -1,10 +1,11 @@
 function Header() {
   return (
     <header>
-      <img src="./src/assets/globe.png" alt="Globe" />
+      <img src="../src/assets/globe.png" alt="Globe" />
       <h1>my travel journal</h1>
     </header>
   );
 }
 
 export default Header;
+
